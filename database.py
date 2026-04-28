aa0e548e2dd17c5ed51f8619b3640d94624ef011
Karolaine Helena Santos Silva <153235224+Karolaine231@users.noreply.github.com>
2026-04-28 15:46:54 -0300

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import os
 
 load_dotenv()
 
-engine = create_engine(os.getenv("DB_URL"), pool_pre_ping=True)
+engine = create_engine(os.getenv("postgresql://monitoramento_dumw_user:NMIIDAwbUTDSP9Z4HEcC1qUhdKpPQgfG@dpg-d7f4mmhj2pic73938udg-a.oregon-postgres.render.com/monitoramento_dumw"), pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
